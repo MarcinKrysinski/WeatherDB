309329716b6f6f1cf966d0430b91919584e11839
Marcin <marcin.jakubkrysinski@gmail.com>
2020-11-17 14:13:43 +0100

    Initial commit

---
+++ src/test/java/pl/krysinski/weatherdb/WeatherDbApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.krysinski.weatherdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
